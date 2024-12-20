f3613435ddbff343e180ccc13f6e2232a7e5be3c
janos1000 <birinyij@kkszki.hu>
2024-12-20 11:00:55 +0100

    Program is created.

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ListViewWithDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(74, 55);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(121, 97);
+            this.listView2.TabIndex = 0;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listView2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ListViewWithDatabase
+{
+    public partial class Form1 : Form
+    {
+        
+        private string connectionString = @"Data Source=YOUR_SERVER;Initial Catalog=user;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDataFromDatabase()
+        {
+            try
+            {
+                
+                string query = "SELECT ID, FirstName, LastName, Password, CreatedTime, UpdateTime FROM data";
+
+                
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+
+                    
+                    DataTable dataTable = new DataTable();
+
+                    
+                    dataAdapter.Fill(dataTable);
+
+                    
+                    listView2.View = View.Details;  
+                    listView2.Columns.Clear();      
+
+                    
+                    listView2.Columns.Add("ID", 50);
+                    listView2.Columns.Add("First Name", 100);
+                    listView2.Columns.Add("Last Name", 100);
+                    listView2.Columns.Add("Password", 100);
+                    listView2.Columns.Add("Created Time", 120);
+                    listView2.Columns.Add("Update Time", 120);
+
+                    
+                    listView2.Items.Clear();
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        ListViewItem item = new ListViewItem(row["Id"].ToString());
+                        item.SubItems.Add(row["FirstName"].ToString());
+                        item.SubItems.Add(row["LastName"].ToString());
+                        item.SubItems.Add(row["Password"].ToString());
+                        item.SubItems.Add(Convert.ToDateTime(row["CreatedTime"]).ToString("yyyy-MM-dd HH:mm:ss"));
+                        item.SubItems.Add(Convert.ToDateTime(row["UpdateTime"]).ToString("yyyy-MM-dd HH:mm:ss"));
+
+                        
+                        listView2.Items.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt: " + ex.Message);
+            }
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
